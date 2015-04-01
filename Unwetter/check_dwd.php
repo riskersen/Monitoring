8be3b58de6fbc7b99f239a1135f307257adea4ac
Oliver Skibbe <oliskibbe@gmail.com>
2015-04-01 07:46:52 +0200

    * check_dwd.php: added warn/crit
      - added regex for warning and critical
      - code cleanup
      - added perf data

--- Unwetter/check_dwd.php
+++ Unwetter/check_dwd.php
@@ -10,21 +10,32 @@ This plugin checks the DWD Web Output for a given Region
  Release 1.0 (2015-03-31)
  - initial release
 
+ Release 1.1 (2015-04-01)
+ - modified REGEX to support CRIT and WARN (UNWETTERWARNUNG > WARNUNG)
+ - code clean up
+
 */
 
 if ( $argc <= 2 ) {
 	help();
 }
 
-$region = ( empty($argv[1]) ) ? "HAN" : strtoupper($argv[1]);
-$region_name = ( empty($argv[2]) ) ? "Hannover" : $argv[2];
+$region = strtoupper($argv[1]);
+$region_name = $argv[2];
 
 $url = "http://www.dwd.de/dyn/app/ws/html/reports/" . $region . "_warning_de.html";
 
-$regex = "@.*Es (sind|ist) (?<warnung_count>\d+) Warnung.*Amtliche UNWETTERWARNUNG (?<warnung>.*) </p>.*von: (?<von_datum>\w+, \d{2}\.\d{2}\.\d{4} \d{2}:\d{2} Uhr) </p>.*bis: (?<bis_datum>\w+, \d{2}\.\d{2}\.\d{4} \d{2}:\d{2} Uhr) </p>.*@isUm";
+$crit_regex = "@.*Es (sind|ist) (?<warnung_count>\d+) Warnung.*Amtliche UNWETTERWARNUNG (?<warnung>.*) </p>.*von: (?<von_datum>\w+, \d{2}\.\d{2}\.\d{4} \d{2}:\d{2} Uhr) </p>.*bis: (?<bis_datum>\w+, \d{2}\.\d{2}\.\d{4} \d{2}:\d{2} Uhr) </p>.*@isUm";
+$warn_regex = "@.*Es (sind|ist) (?<warnung_count>\d+) Warnung.*Amtliche (?<warnung>.*) </p>.*von: (?<von_datum>\w+, \d{2}\.\d{2}\.\d{4} \d{2}:\d{2} Uhr) </p>.*bis: (?<bis_datum>\w+, \d{2}\.\d{2}\.\d{4} \d{2}:\d{2} Uhr) </p>.*@isUm";
 
-$output = "OK: keine Warnungen für " . $region_name .  " auf " . $url . " gefunden|'aktive_warnungen'=0";
-$out_state = 0;
+
+$nagios_return = Array( 
+			0 => "OK",
+			1 => "WARNING",
+			2 => "CRITICAL",
+			3 => "UNKNOWN",
+);
+
 
 // create curl resource 
 $ch = curl_init(); 
@@ -43,23 +54,29 @@ if ( curl_errno($ch) || curl_error($ch) ) {
 	$out_state = 3;
 
 } else {
-
-	$preg_return = preg_match($regex, $dwd_output, $matches);
-	if ( $preg_return ) {
-
-		$output = "CRITICAL: " . $matches['warnung_count'] . " Warnung(en) für " . $region_name . " gefunden, " . $matches['warnung'] . " von: " . $matches['von_datum'] . " bis: " . $matches['bis_datum'] . PHP_EOL . "URL: " . $url;
-		$perf = "'aktive_warnungen'=" . $matches['warnung_count'];
+	// crit regex
+	if ( preg_match($crit_regex, $dwd_output, $matches) ) {
 		$out_state = 2;
+	// warn regex
+	} else if ( preg_match($warn_regex, $dwd_output, $matches) ) {
+		$out_state = 1;
 	} else {
-		$output = "UNKNOWN: output does not match given regex";
-		$out_state = 3;
+		$out_state = 0;
 	}
+
+	if ( $out_state > 0 ) {
+		$output = $matches['warnung_count'] . " Warnung(en) für " . $region_name . " gefunden, " . $matches['warnung'] . " von: " . $matches['von_datum'] . " bis: " . $matches['bis_datum'];
+	} else {
+		$output = "keine Warnungen für " . $region_name .  " auf " . $url . " gefunden";
+	}
+	
+	$perf = sprintf("'aktive_warnungen'=%s", ( $matches['warnung_count'] !== '' ) ? $matches['warnung_count'] : 0 );
 }
 
 // close curl resource to free up system resources 
 curl_close($ch);
 
-echo $output . "|" . $perf;
+echo $nagios_return[$out_state] . ": " . $output . PHP_EOL . "URL: " . $url . "|" . $perf;
 exit($out_state);
 
 function help() {
