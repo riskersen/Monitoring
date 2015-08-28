9baf7e0f8c3d881f86f473bdf434bdf756eb805d
Oliver Skibbe <oliskibbe@gmail.com>
2015-08-28 09:39:42 +0200

    * check_mssql_jobs.php
      - switched from deprecated mssql_ stuff to PDO dblib
      - code cleanup

--- mssql/check_mssql_jobs.php
+++ mssql/check_mssql_jobs.php
@@ -1,5 +1,63 @@
 #!/usr/bin/php -q
 <?php
+/*
+ COPYRIGHT:
+
+ This software is Copyright (c) 2015 Oliver Skibbe
+                                <oliskibbe@gmail.com>
+      (Except where explicitly superseded by other copyright notices)
+
+ LICENSE:
+
+ This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Dieses Programm ist Freie Software: Sie können es unter den Bedingungen
+    der GNU General Public License, wie von der Free Software Foundation,
+    Version 3 der Lizenz oder (nach Ihrer Wahl) jeder neueren
+    veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+
+    Dieses Programm wird in der Hoffnung, dass es nützlich sein wird, aber
+    OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+    Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+    Siehe die GNU General Public License für weitere Details.
+
+    Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+    Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+
+
+
+        Author: Oliver Skibbe (oliskibbe@gmail.com)
+        URL: https://github.com/riskersen/Monitoring / http://oskibbe.blogspot.com
+        Date: 2015-07-28
+        Version: 1.0
+
+        Changelog
+                - 2015-07-20 (Oliver Skibbe): initial release
+                - 2015-07-28 (Oliver Skibbe):
+                        - performance improvements
+                        - added expiry date check
+                                - options x,y control warn/crit
+                                - option d date output format
+*/
+
+if (!extension_loaded('pdo_dblib')) {
+    if (!dl('pdo_dblib.so')) {
+        echo "CRITICAL: extension pdo_dblib is not available, please install (using ubuntu/debian: php5-sybase, rhel-based [enable EPEL first]: php-mssql)" . PHP_EOL;
+        exit(2);
+    }
+}
+
 
 function strposa($haystack, $needles=array(), $offset=0) {
 	$chr = array();
@@ -31,35 +89,42 @@ $password = $options['p'];
 $failed_jobs = 0;
 $succeeded_jobs = 0;
 
-// mssql connect
-$link = mssql_connect($server,$username,$password);
+$return_stateArray = Array(
+        0 => 'OK',
+        1 => 'WARNING',
+        2 => 'CRITICAL',
+        3 => 'UNKNOWN'
+);
 
-if ( ! $link ) {
-	echo "CRITICAL Connection failed: " . mssql_get_last_message();
-	exit(2);
+
+try {
+	// create db object and connect to db server
+	$dbh = new PDO("dblib:host=" . $server, $username, $password);
+
+	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e ) {
+        echo "UNKNOWN: Failed to get DB handle: " . $e->getMessage() . "\n";
+	exit(3);
 }
 
-// options for mssql
-mssql_query("SET ANSI_NULLS ON", $link);
-mssql_query("SET ANSI_WARNINGS ON", $link);
+$dbh->exec("SET ANSI_NULLS ON");
+$dbh->exec("SET ANSI_WARNINGS ON");
 
-// check sql server version
-$version_query = mssql_query("SELECT @@VERSION");
-$version = mssql_fetch_row($version_query);
-$sqlserver_2000 = strpos($version[0], "8.00.") ? true : false;
-
-if ( $debug ) {	
-	if ( $sqlserver_2000) { echo "SQL Server 2000 gefunden";} else {echo "SQL Server neuer als 2000 gefunden"; }
+try {
+	// execute stored procedure MonitorJobs
+	$query = $dbh->query("EXEC sp_MonitorJobs");
+} catch (PDOException $e ) {
+        echo "UNKNOWN: Failed to exec sp_MonitorJobs: " . $e->getMessage() . "\n";
+	exit(3);
 }
 
-// execute stored procedure MonitorJobs
-$sql = "EXEC sp_MonitorJobs";
-$query = mssql_query($sql, $link);
+$resultSet = $query->fetchAll();
 
-if (mssql_num_rows($query)) {
+if (count($resultSet)) {
 	/* Job Array
 	(
-	    [ServerName] => HYDMEDIA
+	    [ServerName] => DBSERVER
 	    [job_name] => Verteilungscleanup: distribution
 	    [job_status] => Enabled
 	    [last_run_status] => Succeeded
@@ -73,7 +138,7 @@ if (mssql_num_rows($query)) {
 	$succeeded_jobArray = Array();
 	$failed_jobArray = Array();
 
-	while ($job = mssql_fetch_array($query, MSSQL_ASSOC)) {
+	foreach ( $resultSet as $job ) {
 		// proceed if job is not excluded
         	if ( strposa(trim($job['job_name']), $excludeList) === false ) {
 			if ( $job['last_run_status'] != 'Succeeded' ) {
@@ -84,7 +149,7 @@ if (mssql_num_rows($query)) {
 		}
 	}
 
-#	$count_failed_jobs = count($failed_jobArray);
+
 	$succeeded_jobs = ( ! empty($succeeded_jobArray)) ? ( max(array_keys($succeeded_jobArray)) + 1) : 0;
 	$failed_jobs = ( ! empty($failed_jobArray)) ? ( max(array_keys($failed_jobArray)) + 1) : 0;
 
@@ -103,18 +168,12 @@ if (mssql_num_rows($query)) {
 	$return_code = 0;
 }
 
-switch($return_code) {
-        case 1:
-                $return_state = "WARNING: "; break;
-        case 2:
-                $return_state = "CRITICAL: "; break;
-        case 3:
-                $return_state = "UNKNOWN: "; break;
-        default:
-                $return_state = "OK: ";
-}
+// close stmt handler
+$query = null;
+// close dbhandler
+$dbh = null;
 
-echo $return_state . $return_msg . "|'failed_jobs'=" . $failed_jobs . " 'succeeded_jobs'=". $succeeded_jobs . PHP_EOL;
+echo $return_stateArray[$return_code] . ": " . $return_msg . "|'failed_jobs'=" . $failed_jobs . " 'succeeded_jobs'=". $succeeded_jobs . PHP_EOL;
 exit($return_code);
 
 function help() {
