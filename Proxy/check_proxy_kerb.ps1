37ba51d2f40a02097e06f3170bec62075e786c2a
Oliver Skibbe <oliskibbe@gmail.com>
2016-11-16 15:22:44 +0100

    check_proxy_kerb - improved error handling
    Error handling has been improved for:
    - timeouts
    - when run under a service account IE GPO "Prevent running First Run
    Wizard" needs to enabled, this is now properly displayed
    - Return state still stays critical if content or response don't match

--- Proxy/check_proxy_kerb.ps1
+++ Proxy/check_proxy_kerb.ps1
@@ -9,14 +9,19 @@
 #			should be called via NRPE/NSClient++
 #
 #			Return Values for NRPE:
-#			OK (0)        - Expected content and status code matches and response time is okay
-#           WARNING (1)   - response time reached warn level
-#			CRITICAL (2)  - Auth impossible or website returned something strange or crit response time reached
+#			Auth possible and website returns 200 - OK (0)
+#			Auth impossible or website returned something strange - CRITICAL (2)
 #			Script errors - UNKNOWN (3)
 #
 # CHANGELOG:
-# 1.0 2016-07-26 - initial version
-# 1.1 2016-07-27 - added timeout, will be twice crit response time
+# 0.8 2016-07-26 - initial version
+# 0.9 2016-11-16
+# 			     - fixed web timeout
+# 			     - fixed output and return state in case of code matches but 
+#                  content not and vice versa
+#                - Output for GPO 'Prevent running First Run Wizard' needs to be enabled
+#                  this is needed when using a service account which never logs in and
+#                  uses IE
 #
 ##############################################################################
 
@@ -29,130 +34,103 @@ Param(
     [Int]$expected_code,
     [string]$expected_content,
     [Int]$warn_response_time = 15,
-    [Int]$crit_response_time = 30
+    [Int]$crit_response_time = 25
 )
 
 # nagios return stuff
 $returnStates = @{0 = 'OK'; 1 = 'WARNING'; 2 = 'CRITICAL'; 3 = 'UNKNOWN' }
-
 # defaults
 $returnState = 3
 $returnString = ""
 
-$request_timeout = $crit_response_time*2
-
-# we want to measure how long it will take to get the result
 $timeTaken = Measure-Command -Expression {
     Try {
     
             # try to fetch web page via requested proxy
-            $proxyResult = ( Invoke-WebRequest -Proxy http://${proxy_server}:${proxy_port} -ProxyUseDefaultCredentials -TimeoutSec $request_timeout -Uri ${target_url} )
-    #} 
+            $proxyResult = ( Invoke-WebRequest -Proxy http://${proxy_server}:${proxy_port} -ProxyUseDefaultCredentials -Uri ${target_url} -TimeoutSec $crit_response_time )
     } Catch {
         # this is needed for any other statuscode than 200
         $proxyResult = $_.Exception.Response
-        $exception_msg = $_.Exception.Message
+        $errorMessage = $_.Exception.Message
 
-        if ($null -ne $proxyResult ) {
+        If ( $errorMessage -Like "*first-launch configuration is not complete.*" ) {
+            Write-Host "UNKNOWN: GPO 'Prevent running First Run Wizard' needs to be enabled"
+            exit 3
+        } ElseIf ( $errorMessage -Like "*The operation has timed out*" ) {
+            Write-Host "UNKNOWN: Webrequest timed out"
+            exit 3
+        } else {
             # get content of webpage
             $reader = New-Object System.IO.StreamReader($proxyResult.GetResponseStream())
             $reader.BaseStream.Position = 0
             $reader.DiscardBufferedData()
             $content = $reader.ReadToEnd();
         }
-    } # endCatch
-
-} # end timeTaken
-
-
-# response time
-$response_time = $timeTaken.TotalSeconds
-$response_time = [Math]::Round($response_time, 2)
-$perf = "'response_time'=" + $response_time + "s;" + $warn_response_time + ";" + $crit_response_time
-
-if ( $exception_msg -notLike "*timed out*" ) {
-
-    # needs to be declared as Int, otherwise it would trigger enum (403 results in forbidden) on status code != 200
-    $statusCode = [Int]$proxyResult.StatusCode
-
-    # if not coming from exception, use generic Content property
-    if ( $content -isnot [string] ) {
-        $content = $proxyResult.Content
     }
+}
+# Finally {
+
+        # response time
+        $seconds = $timeTaken.TotalSeconds
+        $seconds = [Math]::Round($seconds, 2)
+        $perf = "'response_time'=" + $seconds + "s;" + $warn_response_time + ";" + $crit_response_time
+
+        # needs to be declared as Int, otherwise it would trigger enum (403 results in forbidden) on status code != 200
+        $statusCode = [Int]$proxyResult.StatusCode
+
+        # if not coming from exception, use generic Content property
+        if ( $content -isnot [string] ) {
+            $content = $proxyResult.Content
+        }
     
-    # ------
-    # Comparison of http status code
-    # raise level to CRIT if code doesn't match
-    # ------
-    if ( $expected_code -is [Int] -And $statusCode -ne $expected_code ) {
+        # ------
+        # Comparison of http status code
+        # raise level to CRIT if code doesn't match
+        # ------
+        if ( $expected_code -is [Int] -And $statusCode -ne $expected_code ) {
 
-        $returnString = $returnString + "Code '" + $expected_code + "' didn't match:'" + $statusCode + "'"
-        $codeReturnState = 2
+            $returnString = $returnString + "Code '" + $expected_code + "' didn't match:'" + $statusCode + "'"
+            $returnState = 2
 
-    } elseif ( $expected_code -is [Int] -And $statusCode -eq $expected_code ) {
-        $codeReturnState = 0
-        $returnString = $returnString + "Code matches '" + $expected_code + "'"
+        } elseif ( $expected_code -is [Int] -And $statusCode -eq $expected_code ) {
 
-    } #endExpectedCode
+            $returnState = 0
+            $returnString = $returnString + "Code matches '" + $expected_code + "'"
 
-    # ------
-    # Comparison of content
-    # raise level to CRIT if content doesn't match
-    # ------
-    if ( $expected_content -is [string] -And $expected_content.Length -gt 0 ) {
-        if (  $content -NotLike "*$expected_content*" ) {
-            if ( $returnString.Length -gt 0 ) {
+        }
+
+        # ------
+        # Comparison of content
+        # raise level to CRIT if content doesn't match
+        # ------
+        if ( $expected_content -is [string] -And $expected_content.Length -gt 0 -And $content -NotLike "*$expected_content*" ) {
+
+            if ( $returnString.Length -gt 0 -And $returnState -eq 0 ) {
+                $returnString = $returnString + " but"
+            } Else {
                 $returnString = $returnString + " and"
             }
+
             $returnString = $returnString + " content didn't match filter: '" + $expected_content + "'"
 
-            $contentReturnState = 2
-        } else {
+            $returnState = 2
 
-            if ( $returnString.Length -gt 0 ) {
+        } elseif ( $expected_content -is [string] -And $expected_content.Length -gt 0 -And $content -Like "*$expected_content*" ) {
+
+            if ( $returnString.Length -gt 0 -And $returnState -eq 2) {
+                $returnString = $returnString + " but"
+            } Else {
                 $returnString = $returnString + " and"
             }
-            $returnString = $returnString + " content matches filter: '" + $expected_content + "'"
+            $returnString = $returnString + " content matches " + $expected_content
 
-            $contentReturnState = 0
-        } # endCheckExpectedContent
+            # if response didn't match, we won't lower the return code
+            If ( $returnState -eq 0 ) {
+                $returnState = 0
+            }
+        } 
+        $returnString = $returnStates[$returnState] + ": " + $returnString + "|" + $perf
 
-    } else {
-        $contentReturnState = 0
-    } # endExpectedContent
-
-
-    if ( $response_time -gt $crit_response_time ) {
-
-        $returnState = 2
-        $returnString = $returnString + " response time '" + $response_time + "s' above '" + $crit_response_time + "s'"
-
-    } elseif ( $response_time -gt $warn_response_time ) {
-
-        if ( $returnState -le 2 ) {
-            $returnState = 1
-        }
-        $returnString = $returnString + " response time '" + $response_time + "s' above '" + $warn_response_time + "s'"
-
-    } #endResponseTime
-
-
-    # set final returnState
-    if ( $contentReturnState -eq 2  -Or $codeReturnState -eq 2 ) {
-        $returnState = 2
-    } elseif ( $contentReturnState -eq 1  -Or $codeReturnState -eq 1 ) {
-        $returnState = 1
-    } else {
-        $returnState = 0
-    }
-
-} else {
-    $returnString = "timeout reached for target url: " + $target_url
-    $returnState = 3
-}
-
-
-$returnString = $returnStates[$returnState] + ": " + $returnString + "|" + $perf
-
-Write-Host $returnString
-exit $returnState
+    Write-Host $returnString
+    exit $returnState
+#}
